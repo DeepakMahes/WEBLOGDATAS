e48590666132cd57fbaa6693d45ef8dca654204e
DeepakMSD <113435262+DeepakMahes@users.noreply.github.com>
2024-03-08 09:42:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,15 +7,6 @@ const app=express()
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use('/', linkweb)
-// app.get('/edit', (req, res) => {
-
-//     res.render('edit', { value: value });
-// });
-
-
-// app.use('/weblog', require('./routers/weblogdata'));
-
-
 app.listen(port, () => {
      console.log("server is running in 4000");
    });
